f2830c4bfd849306315528e118a5c47f9f36be5e
Sergiu Voloc <SergiuVoloc@users.noreply.github.com>
2018-05-25 12:30:35 +0300

    functia pentru suma de lei initiala  - aleatorie

--- Suma_random_float.c
+++ Suma_random_float.c
@@ -2,39 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-
-  int random (void) {
+int random (void){
 
     int i;
     time_t t;
     float r;
-   // Intializes random number generator */
-   srand((unsigned) time(&t));
 
-   // Print 5 random numbers from 0 to 32000 */
+    srand((unsigned) time(&t));
 
     r = ((float)rand()/(float)(500)) * 1.0 ;
 
-
-
-
-
     r = ((float)rand()/(float)(100)) * 1.0 ;
-      printf("%.2f\n", r ) ;
+    printf("%.2f\n", r ) ;
 
-
-
-   return 0 ;
+    return 0 ;
 }
 
+int main (){
 
-
-int main ()
-{
-    //int i , n ;
-    //scanf("%d",&n);
     random();
-
-
 }
